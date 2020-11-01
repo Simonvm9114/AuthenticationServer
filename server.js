afc22ba65565b4d5f41de0c64faf9ac06c328a84
Simon van Meegdenburg <clsvm@climber.eu>
2020-11-01 11:29:43 +0100

    Initial boilerplate

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+// const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
